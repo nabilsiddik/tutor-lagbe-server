bf0cc106f35c7eecfed298ae94e8c44953a53809
nabilsiddik <nabilsiddik90@gmail.com>
2025-01-03 13:16:46 +0600

    finished

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
+// const jwt = require('jsonwebtoken')
+// const cookieParser = require('cookie-parser')
 
-// Middleware
+// // Middleware
 app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://user-authentication-30262.web.app',
         'https://user-authentication-30262.firebaseapp.com'
-    ],
-    credentials: true
+    ]
+    // credentials: true
 }))
 app.use(express.json())
-app.use(cookieParser())
+// app.use(cookieParser())
 
-const verifyToken = (req, res, next) => {
-    const token = req.cookies?.token
+// const verifyToken = (req, res, next) => {
+//     const token = req.cookies?.token
 
-    if (!token) {
-        return res.status(401).send({ message: "Unauthorized access" })
-    }
+//     if (!token) {
+//         return res.status(401).send({ message: "Unauthorized access" })
+//     }
 
-    // verify token
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-        }
+//     // verify token
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).send({ message: 'unauthorized access' })
+//         }
 
-        req.user = decoded
+//         req.user = decoded
 
-        next()
-    })
+//         next()
+//     })
 
 
-}
+// }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3u9wf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -71,26 +71,26 @@ async function run() {
 
 
 
-        // Auth related apis
-        app.post('/jwt', (req, res) => {
-            const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
+        // // Auth related apis
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
 
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV=== 'production'
-            })
-                .send({ success: true })
-        })
+        //     res.cookie('token', token, {
+        //         httpOnly: true,
+        //         secure: process.env.NODE_ENV=== 'production'
+        //     })
+        //         .send({ success: true })
+        // })
 
 
-        app.post('/logout', (req, res) => {
-            res.clearCookie('token', {
-                httpOnly: true,
-                secure: process.env.NODE_ENV=== 'production'
-            })
-                .send({ success: true })
-        })
+        // app.post('/logout', (req, res) => {
+        //     res.clearCookie('token', {
+        //         httpOnly: true,
+        //         secure: process.env.NODE_ENV=== 'production'
+        //     })
+        //         .send({ success: true })
+        // })
 
 
         // User related APIs
